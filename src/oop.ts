afe61fbd3af419e8a751e39eaae33430daba01bf
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-07 00:59:49 +0600

    type Guard laren

--- src/oop.ts
+++ src/oop.ts
@@ -9,18 +9,59 @@ class Ride {
     rider: string;
     distance: number;
 
-    constructor(rider:string, distance: number){
-     
-        this.rider= rider;
-        this.distance=30;
+    constructor(rider: string, distance: number) {
+
+        this.rider = rider;
+        this.distance = distance;
     }
 
-    calculateOil(){
+    calculateOil() {
         return this.distance * 0.5
     }
 }
 
-const ride=  new Ride("munna", 30)
+const ride = new Ride("munna", 30)
+const ride2 = new Ride("abule", 400)
 
-console.log(ride);
-console.log(ride.calculateOil());
+// console.log(ride, ride2);
+// console.log(ride.calculateOil());
+// console.log(ride2.calculateOil());
+
+
+
+///type Guard
+
+
+function printId(id: string | number) {
+    if (typeof id === "string") {
+        console.log(id.toUpperCase());
+
+    } else {
+        console.log(id.toFixed(2));
+    }
+}
+
+printId(34);
+
+
+
+
+
+type TDriver = { driverName: string }
+type TAdmin = { role: string }
+
+
+
+function checkUSer(user: TDriver | TAdmin) {
+    if ("role" in user) {
+        console.log(Admin.role)
+    } else {
+        console.log(Driver.driverName)
+    }
+}
+
+
+const Driver: TDriver = { driverName: "mokhles" }
+const Admin: TAdmin = { role: "superAdmin" }
+checkUSer(Driver);
+
