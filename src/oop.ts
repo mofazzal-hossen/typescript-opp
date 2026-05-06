4a9af32473e0637abadfb4833a797cb309da0d8f
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-07 00:06:43 +0600

    what is class blueprint = blueprint is structur convat to object . chenge to boject but not change is structur

--- src/oop.ts
+++ src/oop.ts
@@ -1,2 +1,26 @@
-const message: string = "Hello TypeScript";
-console.log(message);
+
+
+// class is a  blueprint which is used to create instanes --> objectt ber korar machine 
+//OBJECT real instance
+
+// ride 
+
+class Ride {
+    rider: string;
+    distance: number;
+
+    constructor(rider:string, distance: number){
+     
+        this.rider= rider;
+        this.distance=30;
+    }
+
+    calculateOil(){
+        return this.distance * 0.5
+    }
+}
+
+const ride=  new Ride("munna", 30)
+
+console.log(ride);
+console.log(ride.calculateOil());
